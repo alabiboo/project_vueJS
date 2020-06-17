f458a6a1513b836358b4b84f84d6715fb311fd19
alabiboo <aalabiboo@gmail.com>
2020-06-17 14:04:47 +0000

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue !',
+    message2: 'Hello 2',
     link: 'miledoo.net',
     success: false,
     personnes: ['abibou', 'amina', 'jean', 'luc', 'nabilath', 'wakilath', 'assana', 'najwa'],
@@ -9,7 +10,59 @@ var app = new Vue({
   },
   methods : {
   	change: function () {
+  		this.message2 = "Fermé",
   		this.success = true
   	}
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Vous avez affiché cette page le ' + new Date().toLocaleString()
+  }
+})
+
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Apprendre JavaScript' },
+      { text: 'Apprendre Vue' },
+      { text: 'Créer quelque chose de génial' }
+    ]
+  }
+})
+
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js !'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
+
+// Component
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Légumes' },
+      { id: 1, text: 'Fromage' },
+      { id: 2, text: 'Tout ce que les humains sont supposés manger' }
+    ]
   }
 })
